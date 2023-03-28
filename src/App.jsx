169f7fef2b43af6cb2b2e5618b19b437f8f4ae93
Anna Pittman <pittann11@gmail.com>
2023-03-28 14:24:24 -0700

    add styling

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductsContainer from './Components/ProductsContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-gray-800 mb-8">Minimalist Store</h1>
+      <ProductsContainer />
+    </div>
+  );
+}
+
+export default App;
